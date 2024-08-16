bcf1055f82a969aa4795e9cbef432edca73d2c7c
Bx <53805165+92EPk@users.noreply.github.com>
2024-08-16 19:16:47 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,93 @@
 #!/usr/bin/python3
 """
-This python script is sing this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+This module uses the urllib module
+to request data from several Rest API
+endpoints to retrieve todo list data
 """
-
-
-import requests
+import json
 import sys
+from urllib.error import HTTPError, URLError
+from urllib.request import urlopen, Request
 
-if __name__ == "__main__":
 
-    userId = sys.argv[1]
-    user = requests.get(
-            "https://jsonplaceholder.typicode.com/users/{}"
-            .format(userId))
-    name = user.json().get('name')
+def decode_data(read_response, response):
+    """
+    read_response -> data from response.read()
+    response -> actual response object
+    This function extract the content charset and decodes
+    it then returns the decoded data.
+    """
+    character_set = response.headers.get_content_charset()
+    # decode the data
+    decoded_raw_data = read_response.decode(character_set)
+    return decoded_raw_data
 
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
-    totalTasks = 0
-    completedTasks = 0
 
-    for task in todos.json():
-        if task.get('userId') == int(userId):
-            totalTasks += 1
-            if task.get('completed'):
-                completedTasks += 1
-    print("Employee {} is done with tasks({}/{}):".format(
-        name, completedTasks, totalTasks))
-    print('\n'.join(["\t " + task.get('title') for task in todos.json(
-            ) if task.get(
-                'userId') == int(userId) and task.get('completed')]))
+def make_request(url, headers=None):
+    """ Handles Error and returns API response body as well
+    as the response object for ease of extracting more information
+    for example the character_set used in the http headers for
+    ease of decoding
+    """
+    request = Request(url, headers=headers or {})
+    try:
+        with urlopen(request, timeout=10) as response:
+            # print(response.status)
+            return response.read(), response
+    except HTTPError as error:
+        print(error.status, error.reason)
+    except URLError as error:
+        print(error.reason)
+    except TimeoutError:
+        print("Request timed out")
+
+
+def call_rest_endpoints(urls):
+    """
+    Takes in a list of REST API endpoints and retrieves data, parses
+    the data then prints the completed tasks of an employee;
+    the list should contain two urls. The first url should be an endpoints
+    to retrieve the user's full name and the second retrieves their tasks todo list.
+    """
+    emp_full_name = None
+    tasks_list = list()
+    completed_tasks = list()
+    total_tasks = 0
+    try:
+        for index, url in enumerate(urls):
+            body, response = make_request(
+                url,
+                {"User-Agent": "cheserem"}
+            )
+            raw_json_data = decode_data(body, response)
+            if index == 0:
+                emp_full_name = json.loads(raw_json_data).get('name')
+            else:
+                tasks_list = json.loads(raw_json_data)
+                total_tasks = len(tasks_list)
+                completed_tasks = list(
+                    filter(
+                        lambda x: (
+                            x.get('completed')),
+                        tasks_list))
+    except BaseException:
+        pass
+
+    else:
+        print("Employee {} is done with tasks({}/{}):".format(emp_full_name,
+              len(completed_tasks), total_tasks))
+        for task in completed_tasks:
+            print("\t {}".format(task.get('title')))
+
+
+if __name__ == '__main__':
+    file_name = sys.argv[0]
+    num_arguments_passed = len(sys.argv)
+    if num_arguments_passed < 2 or num_arguments_passed > 2:
+        print(f"Usage: {file_name} <employees_user_id>")
+    else:
+        user_id = sys.argv[1]
+        urls = [
+            f"https://jsonplaceholder.typicode.com/users/{user_id}",
+            f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"]
+        call_rest_endpoints(urls)
