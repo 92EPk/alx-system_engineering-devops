bf60f3bb283872f3a52cb4fc6a3b34da7bc33719
Bx <53805165+92EPk@users.noreply.github.com>
2024-08-18 17:43:35 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,12 @@
 """
 Script that queries subscribers on a given Reddit subreddit.
 """
-import json
 import requests
-import sys
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "Ahmed 0x16"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
