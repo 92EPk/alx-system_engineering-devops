a7ec4dff0ffbeb457118bc1ebd243ac47e30e1a5
Bx <53805165+92EPk@users.noreply.github.com>
2024-08-18 16:36:51 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
-
+"""Function"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    # Set the URL for the subreddit
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    # Specify a custom User-Agent to avoid ‘Too Many Requests’ errors
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    
-    try:
-        # Make a GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # If the subreddit does not exist, a 404 status code will be returned
-        if response.status_code == 404:
-            return 0
-        
-        # If the request was successful, parse the JSON response
-        data = response.json()
-        
-        # Return the number of subscribers
-        return data['data']['subscribers']
-    
-    except Exception as e:
-        # In case of any other exceptions, return 0
+    """Return"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
