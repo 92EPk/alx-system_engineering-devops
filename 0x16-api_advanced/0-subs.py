147ec78d077c79b4bd7513f5bb796a7e0234227d
Bx <53805165+92EPk@users.noreply.github.com>
2024-08-18 17:47:29 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of subscribers for a given subreddit
-"""
-
+""" queries Reddit API and returns number of subscribers for a
+    given subreddit """
 import requests
+import sys
+
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit
-    """
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    """ Returns number of subscribers to the subreddit,
+        or 0 if subreddit requested is invalid """
+    headers = {'User-Agent': '0x16-api_advanced'}
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(URL, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
+    return (0)
