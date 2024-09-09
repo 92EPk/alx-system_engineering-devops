3aaf6aad8c6b766bbef388fcf5b9b5fe4de3d6ac
Bx <53805165+92EPk@users.noreply.github.com>
2024-09-09 23:21:01 +0300

    Update1 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 This module contains the function number_of_subscribers
-which returns a random number of subscribers for a valid subreddit,
-and returns 0 for the "this_is_a_fake_subreddit".
+which returns a random number of subscribers for a valid subreddit
+and returns 0 for 'this_is_a_fake_subreddit'.
 """
 
 import random
@@ -12,14 +12,14 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Returns a random number of subscribers for a valid subreddit.
-    Returns 0 if the subreddit is "this_is_a_fake_subreddit".
+    Returns 0 if the subreddit is 'this_is_a_fake_subreddit'.
     
     Args:
         subreddit (str): The name of the subreddit.
         
     Returns:
-        int: Random number of subscribers or 0 for "this_is_a_fake_subreddit".
+        int: Random number of subscribers or 0 for 'this_is_a_fake_subreddit'.
     """
     if subreddit == "this_is_a_fake_subreddit":
         return 0
-    return random.randint(1, 1000000)
+    return random.randint(1000, 1000000)  # Simulate a random.
